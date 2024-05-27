cf54823cfee31f2ce9ddd62e1837a13e30974e03
Tim Andersson <timalexanderandersson@hotmail.com>
2024-05-27 13:21:22 +0000

    added the scope

---
+++ run.py
@@ -0,0 +1,10 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/drive.file"
+]
+
