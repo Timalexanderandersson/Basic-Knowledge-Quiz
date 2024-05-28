b7815b3aae661d8d24d7e540b3b4addde4d6835f
Tim Andersson <timalexanderandersson@hotmail.com>
2024-05-28 08:15:38 +0000

    reach the API in Google sheets and creat a function game_playing with some print text for the start of the game

--- run.py
+++ run.py
@@ -8,3 +8,20 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file"
 ]
 
+CREDENT = Credentials.from_service_account_file('quizcreds.json')
+CREDS_SCOPE = CREDENT.with_scopes(SCOPE)
+GOOGLESPREAD = gspread.authorize(CREDS_SCOPE)
+SHEET_HERE = GOOGLESPREAD.open("basic_questions")
+
+questions = SHEET_HERE.worksheet('questions')
+
+data = questions.get_all_values()
+
+#print(data)
+
+def game_playing():
+    print('######################################################################')
+    print(f'           Hello and welcome to the Basic Knowledge Quiz!\n\nHere we gonna test some of your knowledge about some basic questions\n\n                 You think you may know the answers?\n\n                        Lets find out then!')
+    print('######################################################################')
+game_playing()
+
