79a64097076f6b511f2ed5d710460b95f6322c27
Tim Andersson <timalexanderandersson@hotmail.com>
2024-05-28 11:32:00 +0000

    created player_nam function, and did a while loop to get the user to type in a name

--- run.py
+++ run.py
@@ -19,9 +19,27 @@ data = questions.get_all_values()
 
 #print(data)
 
-def game_playing():
+def game_playing_text():
+    """ 
+    This function contains the intro text to the quiz game.
+    """
     print('######################################################################')
     print(f'           Hello and welcome to the Basic Knowledge Quiz!\n\nHere we gonna test some of your knowledge about some basic questions\n\n                 You think you may know the answers?\n\n                        Lets find out then!')
     print('######################################################################')
-game_playing()
+    input('                        Press Enter To Play!')
+    player_name()
+       
+def player_name():
+    name_user_here = ''
+    
+    while name_user_here == '':
+        name_user_here = input('Enter in your name to start the game:').strip().lower()
+        if name_user_here.isnumeric():
+            print('You have to enter a name')
+            name_user_here = input('Enter in your name to start the game:').strip().lower()
+            name_user_here = ''
+
+    print(f'Hey there {name_user_here} lets get started with the game!')
+
+game_playing_text()
 
